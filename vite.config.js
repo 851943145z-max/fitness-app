f9b40c67dc49e2450daebf5f97f5bb24fec95820
851943145z-max <851943145z@gmail.com>
2026-05-26 02:47:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // base 告诉 Vite 这个 App 部署在哪个路径下
+  // GitHub Pages 会把 App 放在 /fitness-app/ 子路径，所以要设置这个
+  base: '/fitness-app/',
 })
